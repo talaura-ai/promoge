b3a86737b7093110dd2d9d118fed7912c6a68b6a
talaura-ai <kalim@talaura.ai>
2025-02-22 23:31:38 +0530

    changes in sidemenu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
-import SideMenu from "./Sidemenu";
+import SideMenu from "../components/Sidemenu";
+import { colors } from "../assets/contants/colors";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
   return (
     <Box sx={{ display: "flex", height: "100vh" }}>
-      <Box sx={{ width: "18%", bgcolor: "black", color: "white" }}>
-        <SideMenu />
+      <Box
+        sx={{
+          width: isCollapsed ? "5%" : "250px",
+          bgcolor: colors.black,
+          color: "white",
+          display: { xs: "none", sm: "none", lg: "block" },
+        }}
+      >
+        <SideMenu isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
       </Box>
-      <Box sx={{ flexGrow: 1, bgcolor: "#fff", padding: 2 }}>{children}</Box>
+      <Box
+        sx={{
+          flexGrow: 1,
+          bgcolor: "#fff",
+          width: isCollapsed ? "95%" : "82%",
+        }}
+      >
+        {children}
+      </Box>
     </Box>
   );
 };
